022e72048f51cabf48f653a8181b82954e96a2cc
Troy Neubauer <troyneubauer@gmail.com>
2017-10-23 19:42:25 -0400

    Added abstraction for movement code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeBase.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Created by Troy Neubauer on 10/16/2017.
+ */
+
+public abstract class OpModeBase extends LinearOpMode {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RightAutonomous.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+@Autonomous(name="Test Autonomous", group="K9bot")
+public class RightAutonomous extends BaseOpMode {
+
+
+}
