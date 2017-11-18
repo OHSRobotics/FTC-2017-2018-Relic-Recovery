a0cef84a635df8cf7f75765e7d4e8424d2d800f5
almal <alm.alex2001@gmail.com>
2017-11-18 11:10:43 -0500

    Left Red Autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeftAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeftAutonomous.java
@@ -2,6 +2,8 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+
 @Autonomous(name="Left Autonomous", group="K9bot")
 public class LeftAutonomous extends AutonomousBase {
 
@@ -11,6 +13,23 @@ public class LeftAutonomous extends AutonomousBase {
 
     @Override
     public void runOpMode() throws InterruptedException {
-
+        getVuMark();
+        waitForStart();
+        //extend tail
+        helper.drive(1.0, -12.0, 2);
+        //detect jewel, detect vumark
+        //hit jewel
+        helper.drive(1.0, 12.0, 2);
+        helper.rotate(90, 1.0, 1.5, 1.0);
+        if (vuMark == RelicRecoveryVuMark.RIGHT){
+            helper.goSideways(1.0, -8.0, 3);
+        }
+        else if (vuMark == RelicRecoveryVuMark.CENTER){
+            helper.goSideways(1.0, 16.0, 4);
+        }
+        else if((vuMark ==  RelicRecoveryVuMark.LEFT) || (vuMark == RelicRecoveryVuMark.UNKNOWN)){
+            helper.goSideways(1.0, 20.0, 4);
+        }
+        helper.drive(1.0, -48.0, 12);
     }
 }
