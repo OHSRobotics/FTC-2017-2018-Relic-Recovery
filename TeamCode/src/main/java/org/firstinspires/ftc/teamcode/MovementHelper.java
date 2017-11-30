1951211e65955e9a71f500d43526f7ed580b048d
Zachary Wang <zachwang101@gmail.com>
2017-11-30 18:14:02 -0500

    updated methods to use getposition instead of run to position to get accurate distance readings, first iteration of a rotation method that utilizes the servo. Updated robot class to include servo. Also tweaked the servo grab values and autonomous base constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MovementHelper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MovementHelper.java
@@ -60,7 +60,12 @@ public class MovementHelper{
         double turnTargetR = -(15.65 * Math.PI * (degrees / 360));
         double turnTargetL = -turnTargetR;
         */
+
         robot.gyro.calibrate();
+        while(robot.gyro.isCalibrating()){
+            opMode.telemetry.addData("chill out yo", "calibrating");
+            opMode.telemetry.update();
+        }
         for(DcMotor motor : robot.motors){
             motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
@@ -86,12 +91,12 @@ public class MovementHelper{
         }*/
         int heading = 0;
         while (opMode.opModeIsActive() && Math.abs(heading - degrees) > 3){
-            heading = robot.gyro.getHeading();
-            if(degrees < 0)
-                heading -= 360;
+            heading = -robot.gyro.getIntegratedZValue();
+            //if(degrees > 0)
+            //    heading = -(robot.gyro.getHeading() - 360);
             opMode.telemetry.addData("target", degrees);
             opMode.telemetry.addData("current", heading);
-            opMode.telemetry.addData("offset", Math.abs(robot.gyro.getHeading() - degrees));
+            opMode.telemetry.addData("offset", Math.abs(heading - degrees));
             opMode.telemetry.update();
         }
         for(DcMotor motor : robot.motors){
@@ -133,18 +138,11 @@ public class MovementHelper{
         }
     }
 
-    public void drive(double speed, double targetDistance, double timeoutS){
+    public void drive(double speed, double targetDistance){
 
         if(targetDistance < 0) {
             speed = -Math.abs(speed);
         }
-        int target;
-        runtime.reset();
-        opMode.telemetry.addData("test from drive!", "");
-        opMode.telemetry.addData("is active", opMode.opModeIsActive());
-        opMode.telemetry.addData("time", (runtime.seconds() < timeoutS));
-        opMode.telemetry.update();
-
         if (opMode.opModeIsActive()) {
             opMode.telemetry.addData("", "op mode active!");
             opMode.telemetry.update();
@@ -180,63 +178,8 @@ public class MovementHelper{
         }
     }
 
-    public void encoderDrive(double speed,
-                             double leftInches, double rightInches,
-                             double timeoutS) {
-        int newLeftTarget;
-        int newRightTarget;
+    public void diaganolDrive(double speed, int targetdistance){
 
-        // Ensure that the opmode is still active
-        if (opMode.opModeIsActive()) {
-
-            // Determine new target position, and pass to motor controller
-            newLeftTarget = robot.leftDrive.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
-            newRightTarget = robot.rightDrive.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
-            robot.leftDrive.setTargetPosition(newLeftTarget);
-            robot.rightDrive.setTargetPosition(newRightTarget);
-
-            // Turn On RUN_TO_POSITION
-            robot.leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            robot.rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-            // reset the timeout time and start motion.
-            runtime.reset();
-            robot.leftDrive.setPower(Math.abs(speed));
-            robot.rightDrive.setPower(Math.abs(speed));
-
-            // keep looping while we are still active, and there is time left, and both motors are running.
-            // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
-            // its target position, the motion will stop.  This is "safer" in the event that the robot will
-            // always end the motion as soon as possible.
-            // However, if you require that BOTH motors have finished their moves before the robot continues
-            // onto the next step, use (isBusy() || isBusy()) in the loop test.
-            while (opMode.opModeIsActive() &&
-                    (runtime.seconds() < timeoutS) &&
-                    (robot.leftDrive.isBusy() && robot.rightDrive.isBusy())) {
-
-                // Display it for the driver.
-                opMode.telemetry.addData("Path1",  "Running to %7d :%7d", newLeftTarget,  newRightTarget);
-                opMode.telemetry.addData("Path2",  "Running at %7d :%7d",
-                        robot.leftDrive.getCurrentPosition(),
-                        robot.rightDrive.getCurrentPosition());
-                opMode.telemetry.update();
-            }
-
-            // Stop all motion;
-            robot.leftDrive.setPower(0);
-            robot.rightDrive.setPower(0);
-
-            // Turn off RUN_TO_POSITION
-            robot.leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            robot.rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-            robot.leftDrive.setPower(0);
-            robot.leftBack.setPower(0);
-            robot.rightDrive.setPower(0);
-            robot.rightBack.setPower(0);
-
-            //  sleep(250);   // optional pause after each move
-        }
     }
 
 }
