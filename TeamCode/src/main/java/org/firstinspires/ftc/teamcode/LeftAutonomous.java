6ec697f184d5d271b51c36f79eb609d0c9fa0d16
Troy Neubauer <troyneubauer@gmail.com>
2017-11-30 18:32:59 -0500

    Removed sleep statement

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeftAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeftAutonomous.java
@@ -14,26 +14,14 @@ public class LeftAutonomous extends AutonomousBase {
     @Override
     public void runOpModeImpl() {
         //extend tail
-        telemetry.addData("starting first drive!", "");
-        telemetry.update();
         helper.drive(0.1, -12.0);
-        telemetry.addData("Done with first drive!", "");
-        telemetry.update();
-        sleep(5000);
+
         boolean leftRed = vision.leftRed();
         //hit jewel
-        telemetry.addData("starting second drive!", "");
-        telemetry.update();
+
         helper.drive(1.0, 12.0);
-        telemetry.addData("Done with second drive!", "");
-        telemetry.update();
-        sleep(5000);
-        telemetry.addData("starting rotate!", "");
-        telemetry.update();
+
         helper.rotate(90, 1.0);
-        telemetry.addData("Done with rotate!", "");
-        telemetry.update();
-        sleep(5000);
 
         if (vuMark == RelicRecoveryVuMark.RIGHT){
             helper.goSideways(1.0, -8.0);
